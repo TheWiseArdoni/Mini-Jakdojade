7ac3135a9aa31d787962807d794ab8d5f930d06a
Tymon <tymon.szczepanski@bsr.edu.pl>
2023-03-02 10:52:07 +0100

    Working version

--- app/src/main/java/com/example/jakdojade/MainActivity.kt
+++ app/src/main/java/com/example/jakdojade/MainActivity.kt
@@ -1,31 +1,34 @@
 package com.example.jakdojade
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.RadioButton
+import androidx.appcompat.app.AppCompatActivity
 import com.example.jakdojade.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.button.setOnClickListener{calculateFare()}
     }
-    fun calculateFare(){
+
+    private fun calculateFare(){
         var destination = binding.textField.text
         val selectedId = binding.discount.checkedRadioButtonId
-        val fare: Double = when (destination.toString()) {
-                "Gdansk" -> 100.0
-                "Krakow" -> 80.0
-            else -> 0.0
-        }
         val discount: Double= when (selectedId) {
             R.id.option_1 -> 0.20
             R.id.option_2-> 0.18
             else -> 1.0
         }
-
+        val fare: Double = when (destination.toString()) {
+                "Gdansk" -> 100.0 - (100.0 * discount)
+                "Krakow" -> 80.0 - (80.0 * discount)
+            else -> 0.0
+        }
+        val stringFare = fare.toString()
+        binding.textView.text = getString(R.string.fare_cost, stringFare)
     }
+
+
 }
